6b720f58483781e39e1faad41fb48207a659363c
Mayon85 <62755258+Mayon85@users.noreply.github.com>
2020-04-14 12:27:41 +0200

    organisation dossiers, essai modal balade

---
+++ js/balades.js
@@ -0,0 +1,39 @@
+
+/*** PAGE BALADES MODALS INCONTOURNABLE ********************************/
+
+// Get the modal
+var modal = document.getElementById("Modal-incontournable");
+
+// Get the button that opens the modal
+var btn = document.getElementById("Btn-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
+// Essai pour faire une boucle
+
+for (var i = 0; i < modal.length; i++) {
+    var modal = modal[i];
+
+    btn.onclick = function(evt) {
+      console.log(evt);
+      modal.style.display = "block";
+    }
+  }
