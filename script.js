60f669a0647cc10cf7f39b12bb052a020d65d1fb
Mayon85 <62755258+Mayon85@users.noreply.github.com>
2020-04-14 08:56:19 +0200

    essai modal balades

--- script.js
+++ script.js
@@ -1,75 +1,4 @@
-/*** JS PAGE PRESENTATION DE L'ÎLE */
-
-/*** IMG CHATEAU ********************************/
-// Get the modal
-var modal = document.getElementById("myModal-chateau");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("histoire-img");
-var modalImg = document.getElementById("img-chateau");
-var captionText = document.getElementById("caption-chateau");
-img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-/*** IMG VIE ISLAISE ********************************/
-// Get the modal
-var modal = document.getElementById("myModal-vie");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("vie-img");
-var modalImg = document.getElementById("img-fetefleur");
-var captionText = document.getElementById("caption-vie");
-img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-/*** IMG BATEAU ********************************/
-// Get the modal
-var modal = document.getElementById("myModal-vivre");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("venir-img");
-var modalImg = document.getElementById("img-bateau");
-var captionText = document.getElementById("caption-bateau");
-img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-
-
-
-/*** LIGHTBOX PETITE GALERIE PHOTOS ********************************/
+/*** PAGE PRESENTATION DE L'ÎLE - SMALL GALLERY *********/
 
 // Open the Modal
 function openModal() {
@@ -117,32 +46,30 @@ function showSlides(n) {
 }
 
 
-
-
-/*** MODALS POUR BALADES ********************************/
+/*** PAGE BALADES MODALS IMG ********************************/
 
 // Get the modal
-var modal = document.getElementById("myModal-incontournable");
+var modal = document.getElementById("Modal-incontournable");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn-incontournable");
+var btn = document.getElementById("Btn-modal");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function () {
-    modal.style.display = "block";
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
+span.onclick = function() {
+  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
